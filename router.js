46cdc3a28cd7848558d7feed38bfea8b5acd190f
Natalia Soazo <80857785+NataliaSoazo@users.noreply.github.com>
2026-04-11 17:27:16 -0300

    archivo principal
    Archivo principal más vista principal

---
+++ router.js
@@ -0,0 +1,24 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+// 🔥 Esto reemplaza __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+//app.use(express.static('vistas'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'vistas', 'Home', 'inicio.html'));
+});
+
+app.listen(3030, () => { console.log("corriendo") });
