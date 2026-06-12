3d1bf74dc8786afe4ce3f36f21fa31a9e7e6097a
Natalia Soazo <80857785+NataliaSoazo@users.noreply.github.com>
2026-06-12 20:21:04 -0300

    CONEXION

--- models/conexion.js
+++ models/conexion.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const sequelizeFotaza = new Sequelize('fotaza_soazo','root','',{
-    host : 'localhost',
+const sequelizeFotaza = new Sequelize(process.env.DATABASE_URL{
+   // host : 'localhost',
     dialect : 'mysql',
     pool: {
       max: 100,         
@@ -10,6 +10,16 @@ const sequelizeFotaza = new Sequelize('fotaza_soazo','root','',{
       idle: 10000     // Tiempo máximo en milisegundos que una conexión puede estar inactiva antes de ser liberada
   },
 });
+/*const sequelizeFotaza = new Sequelize('fotaza_soazo','root','',{
+    host : 'localhost',
+    dialect : 'mysql',
+    pool: {
+      max: 100,         
+      min: 0,       
+      acquire: 30000, // Tiempo máximo en milisegundos para obtener una conexión del pool
+      idle: 10000     // Tiempo máximo en milisegundos que una conexión puede estar inactiva antes de ser liberada
+  },
+});*/
 sequelizeFotaza.authenticate()
   .then(() => {
     console.log('Conectado a MySQL');
